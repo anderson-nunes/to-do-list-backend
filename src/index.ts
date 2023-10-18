447e2017e643d75e2e40463c5ad0d4c6fc4ef90e
anderson-nunes <andersonribeironunes@hotmail.com>
2023-10-18 15:21:29 -0300

    DELETE user implementado

--- src/index.ts
+++ src/index.ts
@@ -141,3 +141,38 @@ app.post("/users", async (req: Request, res: Response) => {
     }
   }
 });
+
+app.delete("/users/:id", async (req: Request, res: Response) => {
+  try {
+    const idToDelete = req.params.id;
+
+    if (idToDelete[1] !== "f") {
+      res.status(404);
+      throw new Error("id deve iniciar com a letra f");
+    }
+
+    const [userIdAlReadyExists]: TUserDB[] | undefined[] = await db(
+      "users"
+    ).where({ id: idToDelete });
+
+    if (!userIdAlReadyExists) {
+      res.status(404);
+      throw new Error("id não encontrado");
+    }
+
+    await db("users").del().where({ id: idToDelete });
+    res.status(200).send({ message: "Usuário deletado com sucesso" });
+  } catch (error) {
+    console.log(error);
+
+    if (req.statusCode === 200) {
+      res.status(500);
+    }
+
+    if (error instanceof Error) {
+      res.send(error.message);
+    } else {
+      res.send("Erro inesperado");
+    }
+  }
+});
